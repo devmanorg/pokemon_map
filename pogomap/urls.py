c05346165e2e297006fb15c91a89a42784a88632
dvmn-tasks <devman-tasks@yandex.ru>
2019-07-18 19:18:59 +0300

    Add settings for ImageField

--- pogomap/urls.py
+++ pogomap/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -8,3 +10,7 @@ urlpatterns = [
     path('', views.show_all_pokemons, name="mainpage"),
     path('pokemon/<pokedex_no>/', views.show_pokemon, name="pokemon"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
