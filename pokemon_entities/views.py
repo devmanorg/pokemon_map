39225170903b5aa04db4122d713523380dfe08bd
Maxim-Pekov <64793556+Maxim-Pekov@users.noreply.github.com>
2022-10-04 15:12:59 +0300

    show pokemons on mainpage

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,6 +3,7 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from .models import Pokemon, PokemonEntity
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -38,13 +39,14 @@ def show_all_pokemons(request):
                 pokemon_entity['lon'],
                 pokemon['img_url']
             )
+    pokemonss = Pokemon.objects.all()
 
     pokemons_on_page = []
-    for pokemon in pokemons:
+    for pokemon in pokemonss:
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': pokemon.image.url,
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
