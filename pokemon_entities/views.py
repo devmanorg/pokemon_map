85e02e0cb006e1603df60feb08bc3555bf7499b2
Maxim-Pekov <max.pekov@gmail.com>
2022-10-13 00:42:35 +0300

    refactoring code

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,6 +1,5 @@
 import folium
 from django.utils.timezone import localtime
-from django.http import HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404
 from .models import Pokemon, PokemonEntity
 
@@ -29,7 +28,6 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 def show_all_pokemons(request):
     pokemon_entities = PokemonEntity.objects.filter(appeared_at__lt=localtime(), disappeared_at__gt=localtime())
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-
     for pokemon_entity in pokemon_entities:
         add_pokemon(
             folium_map, pokemon_entity.latitude,
