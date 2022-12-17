15cbc602b3725fa0d6219678e9699d4932ff643a
Polina <75391420+wezbicka@users.noreply.github.com>
2022-12-17 21:57:22 +0300

    Added relation to db models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -10,6 +10,7 @@ class Pokemon(models.Model):
     
 
 class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     latitude = models.FloatField('Широта')
     longitude = models.FloatField('Долгота')
     
