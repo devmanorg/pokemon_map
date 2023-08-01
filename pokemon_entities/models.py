d063325f68d2506a08d83181604031a8d4880987
romanulanov <turnsourcream@gmail.com>
2023-08-01 11:24:05 +0400

    Add relation to db models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
     title = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='images', null=True)
+    photo = models.ImageField(upload_to='images', null=True, blank=True)
     def __str__(self):
         return '{}'.format(self.title)
     
