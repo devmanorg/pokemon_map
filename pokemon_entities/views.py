85016a7fcb7e837d5c8f770525caa0e026e35b55
romanulanov <turnsourcream@gmail.com>
2023-08-01 12:25:53 +0400

    show pokemons on mainpage

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,6 +3,7 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from .models import Pokemon
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -40,12 +41,13 @@ def show_all_pokemons(request):
             )
 
     pokemons_on_page = []
-    for pokemon in pokemons:
+    for pokemon in Pokemon.objects.all():
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': request.build_absolute_uri(f'/media/{pokemon.photo}'),
+            'title_ru': pokemon.title,
         })
+    print(pokemons_on_page)
 
     return render(request, 'mainpage.html', context={
         'map': folium_map._repr_html_(),
